6e8dfc38e49cf9dc3c1420028a990d2459ef5977
Robert Dominguez <rtjdominguez@gmail.com>
2022-03-08 11:24:17 -0600

    first try at full-screen bg w/o next/Image

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import Head from "next/head";
 import styled from "styled-components";
 
@@ -9,28 +11,42 @@ export default function Home() {
           The Altamont School <span>Student Publications</span>
         </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          consectetur, nisi sed consectetur sagittis, nisl nunc aliquet purus,
-          eget consectetur nisl nunc eget purus.
+          At The Altamont School, we embrace the importance of student voice
+          through written and visual works. We strive to provide a safe
+          environment for student self-expression and to encourage students to
+          express themselves through writing and art. Below, you'll find our
+          three student publications and our students' excellent work.
         </p>
         <Gallery>
           <Item>
             <h2>The Acta Diurna</h2>
             <p>
-              General info about the publication. Little blurb for the visitors.
+              School Newspaper with information about student life, sports, and
+              happenings on the hill.
             </p>
+            <Link href='/acta-diurna'>
+              <a>Read &rarr;</a>
+            </Link>
           </Item>
           <Item>
             <h2>The Beacon</h2>
             <p>
-              General info about the publication. Little blurb for the visitors.
+              Our academic journal and newest publication documenting the
+              feelings and beliefs of the times.
             </p>
+            <Link href='/beacon'>
+              <a>Read &rarr;</a>
+            </Link>
           </Item>
           <Item>
             <h2>The Dragon</h2>
             <p>
-              General info about the publication. Little blurb for the visitors.
+              Our annual literary publication and oldest tradition of
+              documenting students' creative expressions.
             </p>
+            <Link href='/dragon'>
+              <a>Read &rarr;</a>
+            </Link>
           </Item>
         </Gallery>
       </div>
@@ -41,10 +57,23 @@ export default function Home() {
 const LandingWrapper = styled.div`
   display: grid;
   place-items: center center;
-  min-height: 80vh;
+  min-height: 100vh;
+  margin-top: -10vh;
+  padding-top: 10vh;
+  color: white;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.6),
+      rgba(0, 0, 0, 0.6)
+    ),
+    url("./landing_bg.jpeg");
+  background-size: cover;
+  background-position: center;
+
   h1 {
     font-size: var(--hero);
     font-weight: 400;
+    margin-bottom: 1vh;
 
     span {
       color: var(--accent);
@@ -57,6 +86,7 @@ const Gallery = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 1rem;
+  margin-top: 5vh;
   place-items: center center;
 `;
 
@@ -65,7 +95,19 @@ const Item = styled.div`
   flex-direction: column;
   padding: 1rem;
   width: 100%;
-  border: solid 1px var(--black);
+  border: solid 1px white;
   border-radius: 5px;
   text-align: center;
+
+  a {
+    text-decoration: none;
+    color: white;
+    background: var(--dark-grey);
+    border-radius: 2px;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background: var(--light-grey);
+    }
+  }
 `;
