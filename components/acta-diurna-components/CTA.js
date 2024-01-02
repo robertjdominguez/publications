38bd3f65c3351f2121ada0ced8aa68bde8b4afc9
Rob Dominguez <rob.dominguez@hasura.io>
2024-01-02 09:44:07 -0600

    UI: fix overflow for CTA

--- components/acta-diurna-components/CTA.js
+++ components/acta-diurna-components/CTA.js
@@ -7,9 +7,7 @@ export default function CTA() {
       <p>
         <i>
           To share your thoughts on this or on anything you've seen in The Acta Diurna, to suggest story ideas, or to
-          become a contributor, email{' '}
-          <a href="mailto:MediaJournalismSpring2023@AltamontSchool.org">MediaJournalismSpring2023@AltamontSchool.org</a>
-          .
+          become a contributor, <a href="mailto:MediaJournalismSpring2023@AltamontSchool.org">email us</a>.
         </i>
       </p>
     </CTAWrapper>
@@ -17,10 +15,12 @@ export default function CTA() {
 }
 
 const CTAWrapper = styled.div`
+  max-width: 100%;
   border-top: 1px solid var(--accent);
+  color: var(--dark-grey);
   @media (max-width: 768px) {
     p {
-      font-size: 1.2rem;
+      font-size: 1rem;
       padding: 0 1rem;
     }
   }
